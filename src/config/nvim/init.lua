a29076895adbd123c3f9c6df59d7656151b25ce5
Cameron Bothner <cameron.bothner@shopify.com>
2024-08-14 14:59:23 -0400

    Fix FZF and cleanup

--- src/config/nvim/init.lua
+++ src/config/nvim/init.lua
@@ -376,61 +376,6 @@ vim.api.nvim_create_autocmd("FileType", {
   command = "execute ':Copilot disable'"
 })
 
--- -- Compe
--- vim.opt.completeopt = "menuone,noselect"
-
--- require'compe'.setup {
---   source = {
---     path = true;
---     buffer = true;
---     calc = true;
---     nvim_lsp = true;
---     nvim_lua = true;
---     vsnip = true;
---     ultisnips = true;
---     luasnip = true;
---   };
--- }
-
--- local t = function(str)
---   return vim.api.nvim_replace_termcodes(str, true, true, true)
--- end
-
--- local check_back_space = function()
---     local col = vim.fn.col('.') - 1
---     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
--- end
-
--- -- Use (s-)tab to:
--- --- move to prev/next item in completion menuone
--- --- jump to prev/next snippet's placeholder
--- _G.tab_complete = function()
---   if vim.fn.pumvisible() == 1 then
---     return t "<C-n>"
---   elseif vim.fn['vsnip#available'](1) == 1 then
---     return t "<Plug>(vsnip-expand-or-jump)"
---   elseif check_back_space() then
---     return t "<Tab>"
---   else
---     return vim.fn['compe#complete']()
---   end
--- end
--- _G.s_tab_complete = function()
---   if vim.fn.pumvisible() == 1 then
---     return t "<C-p>"
---   elseif vim.fn['vsnip#jumpable'](-1) == 1 then
---     return t "<Plug>(vsnip-jump-prev)"
---   else
---     -- If <S-Tab> is not working in your terminal, change it to <C-h>
---     return t "<S-Tab>"
---   end
--- end
-
--- vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
--- vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
--- vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
--- vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-
 -- Fzf
 -- Customize fzf colors to match your color scheme
 -- - fzf#wrap translates this to a set of `--color` options
@@ -438,9 +383,9 @@ vim.g.fzf_colors = {
   fg = { 'fg', 'Normal' },
   bg = { 'bg', 'Normal' },
   hl = { 'fg', 'Comment' },
-  fg_plus = { 'fg', 'CursorLine', 'CursorColumn', 'Normal' },
-  bg_plus = { 'bg', 'CursorLine', 'CursorColumn' },
-  hl_plus = { 'fg', 'Statement' },
+  ["fg+"] = { 'fg', 'CursorLine', 'CursorColumn', 'Normal' },
+  ["bg+"] = { 'bg', 'CursorLine', 'CursorColumn' },
+  ["hl+"] = { 'fg', 'Statement' },
   info = { 'fg', 'PreProc' },
   border = { 'fg', 'Ignore' },
   prompt = { 'fg', 'Conditional' },
